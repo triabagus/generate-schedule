66f8139ae2adeca43b3140602a7d59013295be46
triabagus <triatop9@gmail.com>
2020-02-13 10:49:17 +0700

    add sweet alert delete confirm and add button

--- resources/views/admin-news/time/index.blade.php
+++ resources/views/admin-news/time/index.blade.php
@@ -6,12 +6,21 @@
 
 @section('style')
 <link href="{{ asset('new_template/assets/libs/tablesaw/dist/tablesaw.css') }}" rel="stylesheet">
+<link href="{{ asset('new_template/assets/libs/sweetalert2/dist/sweetalert2.min.css') }}" rel="stylesheet">
+
+<style>
+    form.deleteedition{
+        display:inline-block;
+    }
+</style>
 @stop
 
 @section('script')
 
 <script src="{{ asset('new_template/assets/libs/tablesaw/dist/tablesaw.jquery.js') }}"></script>
 <script src="{{ asset('new_template/assets/libs/tablesaw/dist/tablesaw-init.js') }}"></script>
+<script src="{{ asset('new_template/assets/libs/sweetalert2/dist/sweetalert2.all.min.js') }}" aria-hidden="true"></script>
+<script src="{{ asset('new_template/assets/libs/sweetalert2/sweet-alert.init.js') }}" aria-hidden="true"></script>
 @stop
 
 @section('content')
@@ -27,18 +36,18 @@
             </div>
         </div>
 
-    <div class="col-7 align-self-center">
-        <div class="d-flex no-block justify-content-end align-items-center">
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item">
-                        <a href="{{ route('admin.dashboard') }}">Dashboard</a>
-                    </li>
-                    <li class="breadcrumb-item active" aria-current="page">{{ $title }}</li>
-                </ol>
-            </nav>
+        <div class="col-7 align-self-center">
+            <div class="d-flex no-block justify-content-end align-items-center">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="{{ route('admin.dashboard') }}">Dashboard</a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">{{ $title }}</li>
+                    </ol>
+                </nav>
+            </div>
         </div>
-    </div>
 
     </div>
 </div>
@@ -50,9 +59,31 @@
 <!-- Container fluid  -->
 <!-- ============================================================== -->
 <div class="container-fluid">
+
     @include('admin-news._partials.notifications')
     <div class="card">
         <div class="card-body">
+        <div class="row mb-3">
+            <div class="col-7 align-self-center">
+                <h6 class="card-subtitle">
+                    Description Time blade.
+                </h6>
+                <a class="btn btn-sm btn-success" href="{{ route('admin.time.create') }}">
+                    <i class="ti-plus">
+                    </i>
+                    Tambah
+                </a>
+            </div>
+            <div class="col-5 align-self-center">
+                <div class="d-flex no-block justify-content-end align-items-center">
+                    <a class="btn btn-sm btn-success" href="{{ route('admin.time.create') }}">
+                        <i class="ti-plus">
+                        </i>
+                        Search
+                    </a>
+                </div>
+            </div>
+        </div>
 
         <table class="no-wrap table-bordered table-hover table" data-tablesaw>
             <thead class="bg-info text-white">
@@ -71,13 +102,21 @@
                     <td>{{ $time->time_begin }}</td>
                     <td>{{ $time->time_finish }}</td>
                     <td>{{ $time->range }}</td>
-                    <td>
+                    <td style="width:25px;">
                         <div class="button-group">
-                            <a class="btn btn-primary" href="{{ route('admin.time.edit', $time->id) }}">
+                            <a class="btn btn-sm btn-primary" href="{{ route('admin.time.edit', $time->id) }}">
                                 <i class="ti-pencil">
                                 </i>
                                 Ubah
                             </a>
+
+                            {!! Form::model($time, ['route' => ['admin.time.delete', $time->id], 'id'=> 'delete-'.$time->id, 'class'=> 'deleteedition']) !!}
+                            {!! Form::hidden('_method', 'DELETE') !!}
+                            {!! Form::button('
+                            <i class="ti-trash"></i>
+                            Hapus', ['type' => 'submit', 'class' => 'btn btn-danger btn-sm sa-removeData', 'data-file'=> 'delete-'.$time->id]) !!}
+                            {!! Form::close() !!}
+
                         </div>
                     </td>
                 </tr>
